1e6c86a2d5038aa8bdd2650640952e6092fee0dc
Kim HyunJi <khyunjiee@gmail.com>
2021-05-23 02:26:26 +0900

    update: post 파일 수정

--- src/api/post.ts
+++ src/api/post.ts
@@ -1,6 +1,5 @@
 import express, { Request, Response } from "express";
 import { check, validationResult } from "express-validator";
-import { userInfo } from "os";
 
 import Post from "../models/Post";
 
