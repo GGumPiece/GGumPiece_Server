e23e31cd9e71f2e4ebf45e9b094dcc0ee5de6993
Kim HyunJi <khyunjiee@gmail.com>
2021-05-23 01:06:56 +0900

    create: 프로젝트 초기 세팅

---
+++ src/config/index.ts
@@ -0,0 +1,31 @@
+import dotenv from "dotenv";
+
+// Set the NODE_ENV to 'development' by default
+process.env.NODE_ENV = process.env.NODE_ENV || "development";
+
+const envFound = dotenv.config();
+if (envFound.error) {
+  // This error should crash whole process
+
+  throw new Error("⚠️  Couldn't find .env file  ⚠️");
+}
+
+// 에러가 없는 클린한 구역 -> dotenv에 넣어놨던 정보를 가져올 수 있음
+export default {
+  /**
+   * Your favorite port
+   */
+  port: parseInt(process.env.PORT, 10),
+
+  /**
+   * Your secret sauce
+   */
+   jwtSecret: process.env.JWT_SECRET,
+   jwtAlgorithm: process.env.JWT_ALGO,
+
+  /**
+   * That long string from mlab
+   */
+  mongoURI: process.env.MONGODB_URI,
+
+};
