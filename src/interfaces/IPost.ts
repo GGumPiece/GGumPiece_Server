2ebc93a0e5ed48d85c6d78ae994e0e7406566fc1
Kim HyunJi <khyunjiee@gmail.com>
2021-05-23 05:26:08 +0900

    update: id로 post 조회/삭제 라우터 수정

--- src/interfaces/IPost.ts
+++ src/interfaces/IPost.ts
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 import { IDreamReading } from "./IDreamReading";
 
 export interface IPost {
-  _id?: string;
+  id?: string;
   content: string;
   emoji: string;
   dreamReadings?: [IDreamReading];
