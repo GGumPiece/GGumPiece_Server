56a3f7909bd9256509e2969ae784408db82eb114
bak seol <moonleaf0213@naver.com>
2021-05-23 05:20:35 +0900

    게시글 전체조회 기능 추가

--- src/api/post.ts
+++ src/api/post.ts
@@ -98,4 +98,23 @@ router.delete("/:id", auth, async (req: Request, res: Response) => {
   }
 })
 
+
+
+/**
+ *  @route GET api/posts
+ *  @desc Get all posts
+ *  @access Private
+ */
+//게시글 전체조회
+ router.get("/", auth, async (req: Request, res: Response) => {
+  try {
+    const user = await User.findOne({ id: req.body.user.id }); // const posts = await Post.find().sort({ date: -1 }); 를 User 안에 posts가 있는 것이므로 user의 id를 조회한 다음 그 id에 해당하는 모든 posts를 가져오는 형식
+    const posts = user.posts; // user를 통해서 posts로 접근해서 전체 조회
+    res.json(posts); // 보내줌
+  } catch (error) { // + 포스트 정보 없을 때 404에러 추가
+    console.error(error.message);
+    res.status(500).send("Server Error");
+  }
+});
+
 module.exports = router;
